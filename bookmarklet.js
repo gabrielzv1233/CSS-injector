49ac8373567a70c8a3783eb334f74460e4179d09
gabrielzv1233 <77300296+gabrielzv1233@users.noreply.github.com>
2023-05-15 07:59:25 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,11 @@
-
+javascript:(function() {
+  var cssUrl = prompt('Enter the URL of a CSS file:');
+  if (cssUrl) {
+    var head = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = cssUrl;
+    head.appendChild(link);
+  }
+})();
